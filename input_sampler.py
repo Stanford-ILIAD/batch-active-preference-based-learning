8521e3e1b693dcd49a2276ae898d5acb0ca35499
Erdem Bıyık <ebiyik@stanford.edu>
2018-06-15 04:44:42 -0700

    small patch
    create directory if it does not exist

--- input_sampler.py
+++ input_sampler.py
@@ -12,5 +12,8 @@ lower_input_bound = [x[0] for x in simulation_object.feed_bounds]
 upper_input_bound = [x[1] for x in simulation_object.feed_bounds]
 inputs_set = np.random.uniform(low=2*lower_input_bound, high=2*upper_input_bound, size=(K, 2*z))
 psi_set = generate_psi(simulation_object, inputs_set)
+
+if not os.direxists(os.path.join(os.getwd()), 'ctrl_samples'):
+    os.mkdir('ctrl_samples')
 np.savez('ctrl_samples/' + simulation_object.name + '.npz', inputs_set=inputs_set, psi_set=psi_set)
 print('Done!')
